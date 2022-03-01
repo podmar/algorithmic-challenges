6d6de214c2a6853305e032e75c2b6b4d346829a2
podmar <marta.podziewska@gmail.com>
2022-03-01 21:11:21 +0100

    changed the assertions in palindrome number test file to assertTrue/False

--- test/test_palindrome_number.py
+++ test/test_palindrome_number.py
@@ -7,40 +7,40 @@ class TestPalindrome(unittest.TestCase):
         """Testing the first solution with string approach
         """
 
-        self.assertEqual(string_solution(121), True)
-        self.assertEqual(string_solution(-121), False)
-        self.assertEqual(string_solution(123), False)
-        self.assertEqual(string_solution(-2), False)
-        self.assertEqual(string_solution(10455401), True)
-        self.assertEqual(string_solution(0), True)
-        self.assertEqual(string_solution(50895), False)
-        self.assertEqual(string_solution(44444), True)
+        self.assertTrue(string_solution(121))
+        self.assertFalse(string_solution(-121))
+        self.assertFalse(string_solution(123))
+        self.assertFalse(string_solution(-2))
+        self.assertTrue(string_solution(10455401))
+        self.assertTrue(string_solution(0))
+        self.assertFalse(string_solution(50895))
+        self.assertTrue(string_solution(44444))
 
     def test_log10_solution(self): 
         """Testing the solution with log10
         """
 
-        self.assertEqual(log10_solution(121), True)
-        self.assertEqual(log10_solution(-121), False)
-        self.assertEqual(log10_solution(123), False)
-        self.assertEqual(log10_solution(-2), False)
-        self.assertEqual(log10_solution(10455401), True)
-        self.assertEqual(log10_solution(0), True)
-        self.assertEqual(log10_solution(50895), False)
-        self.assertEqual(log10_solution(44444), True)
+        self.assertTrue(log10_solution(121))
+        self.assertFalse(log10_solution(-121))
+        self.assertFalse(log10_solution(123))
+        self.assertFalse(log10_solution(-2))
+        self.assertTrue(log10_solution(10455401))
+        self.assertTrue(log10_solution(0))
+        self.assertFalse(log10_solution(50895))
+        self.assertTrue(log10_solution(44444))
 
     def test_modulo_solution(self): 
         """Testing the modulo solution
         """
 
-        self.assertEqual(modulo_solution(121), True)
-        self.assertEqual(modulo_solution(-121), False)
-        self.assertEqual(modulo_solution(123), False)
-        self.assertEqual(modulo_solution(-2), False)
-        self.assertEqual(modulo_solution(10455401), True)
-        self.assertEqual(modulo_solution(0), True)
-        self.assertEqual(modulo_solution(50895), False)
-        self.assertEqual(modulo_solution(44444), True)
+        self.assertTrue(modulo_solution(121))
+        self.assertFalse(modulo_solution(-121))
+        self.assertFalse(modulo_solution(123))
+        self.assertFalse(modulo_solution(-2))
+        self.assertTrue(modulo_solution(10455401))
+        self.assertTrue(modulo_solution(0))
+        self.assertFalse(modulo_solution(50895))
+        self.assertTrue(modulo_solution(44444))
 
 
 if __name__ == "__main__": 
