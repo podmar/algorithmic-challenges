b0b5f27e9108bb0bbdd49ff76545178ce27eb0da
podmar <marta.podziewska@gmail.com>
2022-03-01 21:04:52 +0100

    changed the assertions in mountain array test file to assertTrue/False

--- test/test_mountain_array.py
+++ test/test_mountain_array.py
@@ -6,17 +6,16 @@ class TestArray(unittest.TestCase):
     def test_(self): 
         """Testing the solution function to the mountain array challenge
         """
-
-        self.assertEqual(solution([2,1]), False)
-        self.assertEqual(solution([3,5,5]), False)
-        self.assertEqual(solution([0,3,2,1]), True)
-        self.assertEqual(solution([1,2,7,5,2,1]), True)
-        self.assertEqual(solution([7,5,3,1,0]), False)
-        self.assertEqual(solution([1,5,8,9,17]), False)
-        self.assertEqual(solution([1,5,8,9,17,3]), True)
-        self.assertEqual(solution([-1,0,1]), False)
-        self.assertEqual(solution([-15,-3,0,5,1]), True)
-        self.assertEqual(solution([-1,14,20]), False)
+        self.assertFalse(solution([2,1]))
+        self.assertFalse(solution([3,5,5]))
+        self.assertTrue(solution([0,3,2,1]))
+        self.assertTrue(solution([1,2,7,5,2,1]))
+        self.assertFalse(solution([7,5,3,1,0]))
+        self.assertFalse(solution([1,5,8,9,17]))
+        self.assertTrue(solution([1,5,8,9,17,3]))
+        self.assertFalse(solution([-1,0,1]))
+        self.assertTrue(solution([-15,-3,0,5,1]))
+        self.assertFalse(solution([-1,14,20]))
 
 if __name__ == "__main__": 
     unittest.main()
