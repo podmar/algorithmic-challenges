1a2d55380319c31901102203dd87a3df98d351cb
podmar <marta.podziewska@gmail.com>
2022-11-02 15:38:24 +0100

    reversing a number first draft

---
+++ challenges/reverse_integer.py
@@ -0,0 +1,33 @@
+# #Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+# Assume the environment does not allow you to store 64-bit integers(signed or unsigned).
+
+
+# Example 1:
+
+# Input: x = 123
+# Output: 321
+# Example 2:
+
+# Input: x = -123
+# Output: -321
+# Example 3:
+
+# Input: x = 120
+# Output: 21
+
+
+# Constraints:
+
+# -231 <= x <= 231 - 1
+
+# reverse integer
+
+def solution(n):
+    reversed_number = 0
+
+    while n > 0:
+        reversed_number *= 10
+        reversed_number += n % 10
+        n = n//10
+
+    return reversed_number
