be83c262e6666d82366de38c58fde67909699d92
podmar <marta.podziewska@gmail.com>
2022-11-03 22:30:27 +0100

    task content and test setup

---
+++ test/test_string_to_integer_atoi.py
@@ -0,0 +1,13 @@
+import unittest
+
+from challenges.string_to_integer_atoi import solution
+
+class TestStringToInteger(unittest.TestCase):
+    def test_solution(self):
+        """
+        """
+
+        self.assertEqual(solution(),)
+
+if __name__ == "__main__":
+    unittest.main()
