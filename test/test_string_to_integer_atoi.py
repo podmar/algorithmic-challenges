bcbf855723c3c26349bf20d288d1ae2ec8f2a177
podmar <marta.podziewska@gmail.com>
2022-11-04 16:29:39 +0100

    added basic test cases from task description

--- test/test_string_to_integer_atoi.py
+++ test/test_string_to_integer_atoi.py
@@ -3,11 +3,13 @@ import unittest
 from challenges.string_to_integer_atoi import solution
 
 class TestStringToInteger(unittest.TestCase):
-    def test_solution(self):
-        """
+    def test_solution_basic(self):
+        """Checking for basic test cases from task description
         """
 
-        self.assertEqual(solution(),)
+        self.assertEqual(solution("42"), 42)
+        self.assertEqual(solution("   -42"), -42)
+        self.assertEqual(solution("4193 with words"), 4193)
 
 if __name__ == "__main__":
     unittest.main()
