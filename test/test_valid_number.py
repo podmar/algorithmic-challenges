0f21c444168795e457053b97cd61d3811f568d98
podmar <marta.podziewska@gmail.com>
2022-11-04 23:15:21 +0100

    tests for remaining decimal numbers

--- test/test_valid_number.py
+++ test/test_valid_number.py
@@ -21,13 +21,16 @@ class TestValidNumber(unittest.TestCase):
         self.assertFalse(solution("abc"))
         self.assertFalse(solution("/('#122"))
 
-# For example, all the following are valid numbers: ["-0.1", "+3.14", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"], while the following are not valid numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"].
+# For example, all the following are valid numbers: ["", "", "", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"], while the following are not valid numbers: ["abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"].
 
     def test_decimal_number(self):
         """ Tests if the solution works for decimal numbers
         """
 
         self.assertTrue(solution("4."))
+        self.assertTrue(solution("-0.1"))
+        self.assertTrue(solution("+3.14"))
+        self.assertTrue(solution("-.9"))
 
 
 if __name__ == "__main__":
