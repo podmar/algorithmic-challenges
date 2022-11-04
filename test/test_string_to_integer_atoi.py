4287e93cce18c2af0221961fb423471ad6877719
podmar <marta.podziewska@gmail.com>
2022-11-04 19:09:53 +0100

    test for empty string and for negative numbers outside 32bit range

--- test/test_string_to_integer_atoi.py
+++ test/test_string_to_integer_atoi.py
@@ -14,6 +14,7 @@ class TestStringToInteger(unittest.TestCase):
     def test_solution_corner_cases(self):
         """Additional corner cases
         """
+        self.assertEqual(solution(""), 0)
         self.assertEqual(solution("-0"), 0)
         self.assertEqual(solution("  +0"), 0)
         self.assertEqual(solution("  +47"), 47)
@@ -27,6 +28,8 @@ class TestStringToInteger(unittest.TestCase):
         self.assertEqual(solution("-214748364832423"), -2147483648)
         self.assertEqual(solution("  -214748364823324"), -2147483648)
         self.assertEqual(solution("-2357483648"), -2147483648)
+        self.assertEqual(solution("-91283472332"), -2147483648)
+
 
     def test_solution_32bit(self):
         """Testing results outside of the max limit of the 32 bit range
@@ -34,6 +37,9 @@ class TestStringToInteger(unittest.TestCase):
         self.assertEqual(solution("214748364832423"), 2147483647)
         self.assertEqual(solution("214748364823324"), 2147483647)
         self.assertEqual(solution("2357483648"), 2147483647)
+        self.assertEqual(solution("2357483648"), 2147483647)
+
+        
 
 if __name__ == "__main__":
     unittest.main()
