12e64461b8529d55ecf1366679c117e2e92a2a7b
podmar <marta.podziewska@gmail.com>
2022-11-04 18:49:08 +0100

    tests for 32 bit range

--- test/test_string_to_integer_atoi.py
+++ test/test_string_to_integer_atoi.py
@@ -11,5 +11,29 @@ class TestStringToInteger(unittest.TestCase):
         self.assertEqual(solution("   -42"), -42)
         self.assertEqual(solution("4193 with words"), 4193)
 
+    def test_solution_corner_cases(self):
+        """Additional corner cases
+        """
+        self.assertEqual(solution("-0"), 0)
+        self.assertEqual(solution("  +0"), 0)
+        self.assertEqual(solution("  +47"), 47)
+        self.assertEqual(solution("  + 47"), 0)
+        self.assertEqual(solution("   this is no number string"), 0)
+        self.assertEqual(solution("       -00091"), -91)
+
+    def test_solution_32bit(self):
+        """Testing results outside the lower min of the 32 bit range
+        """
+        self.assertEqual(solution("-214748364832423"), -2147483648)
+        self.assertEqual(solution("  -214748364823324"), -2147483648)
+        self.assertEqual(solution("-2357483648"), -2147483648)
+
+    def test_solution_32bit(self):
+        """Testing results outside of the max limit of the 32 bit range
+        """
+        self.assertEqual(solution("214748364832423"), 2147483647)
+        self.assertEqual(solution("214748364823324"), 2147483647)
+        self.assertEqual(solution("2357483648"), 2147483647)
+
 if __name__ == "__main__":
     unittest.main()
