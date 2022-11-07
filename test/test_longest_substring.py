5116fa7c52b702210898b9a3db8bc26b4da7b8c8
podmar <marta.podziewska@gmail.com>
2022-11-07 16:02:10 +0100

    task description and test boilerplate

---
+++ test/test_longest_substring.py
@@ -0,0 +1,13 @@
+import unittest
+
+from challenges.longest_substring import solution
+
+
+class TestLongestSubstring(unittest.TestCase()):
+    def test_output_type(self):
+        """Testing if the output is a boolean.
+        """
+
+
+if __name__ == "__main__":
+    unittest.main()
