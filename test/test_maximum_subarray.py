f105679e3728447fb76d820b9082bac93bacea80
podmar <marta.podziewska@gmail.com>
2022-11-10 20:48:49 +0100

    additional cases

--- test/test_maximum_subarray.py
+++ test/test_maximum_subarray.py
@@ -10,13 +10,21 @@ class TestContainsDuplicate(unittest.TestCase):
 
         self.assertTrue(isinstance(solution([1, 2, 3, 1]), int))
 
-    def test_max_subarray(self):
+    def test_max_subarray_base_examples(self):
         """Testing cases from the examples
         """
 
         self.assertEqual(solution([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6)
         self.assertEqual(solution([1]), 1)
         self.assertEqual(solution([5, 4, -1, 7, 8]), 23)
+
+    def test_max_subarray_additional_cases(self):
+        """Testing additional cases
+        """
+
+        self.assertEqual(solution([7, 4, 0, -9, 1]), 11)
+        self.assertEqual(solution([0]), 0)
+        self.assertEqual(solution([-1, -5, -4]), -1)
 
 
 if __name__ == "__main__":
