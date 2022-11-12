81886a1df33a3b7323098597b3afed15b24b1e93
podmar <marta.podziewska@gmail.com>
2022-11-12 20:30:04 +0100

    removed redundant comments

--- challenges/rain_trap.py
+++ challenges/rain_trap.py
@@ -2,19 +2,17 @@
 
 def solution(array: list[int]) -> int:
 
-    # define the max on the left for each element in the list
-
     rain_volume = 0
 
     if len(array) < 3:
         return rain_volume
 
-    for index in range(1, len(array) - 1):  # i=1, max_l=4,
+    for index in range(1, len(array) - 1):
         max_left = array[0]
         for left_index in range(index):
             max_left = max(array[left_index], max_left)
 
-        max_right = array[-1]  # m_r = 0, mr=3
+        max_right = array[-1]
         for right_index in range(index + 1, len(array) - 1):
             max_right = max(array[right_index], max_right)
 
