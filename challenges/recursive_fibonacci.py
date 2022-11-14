9f2b459c48d4955bdeda77eef784bf8e55eee33a
podmar <marta.podziewska@gmail.com>
2022-11-14 11:50:18 +0100

    recursive solution to fibonacci, do not test on large numbers

--- challenges/recursive_fibonacci.py
+++ challenges/recursive_fibonacci.py
@@ -1,3 +1,13 @@
-def solution(n: int) -> int:
+def fib(n: int) -> int:
     """ The funtion returns the n-th number in the Fibonacci sequence
     """
+    result: int
+
+    if n == 0:
+        result = 0
+    elif n == 1:
+        result = 1
+    else:
+        result = fib(n-2) + fib(n-1)
+
+    return result
--- test/test_recursive_fibonacci.py
+++ test/test_recursive_fibonacci.py
@@ -1,6 +1,6 @@
 import unittest
 
-from challenges.recursive_fibonacci import solution
+from challenges.recursive_fibonacci import fib
 
 
 class TestFibonacci(unittest.TestCase):
@@ -8,11 +8,31 @@ class TestFibonacci(unittest.TestCase):
         """Tests is the output is int
         """
 
-        self.assertTrue(type(solution(3)) == int)
+        self.assertTrue(type(fib(3)) == int)
 
-    def test_(self):
-        """ 
+    def test_basic_cases(self):
+        """ Testing for base cases not requiring recursion
         """
+
+        self.assertEqual(fib(0), 0)
+        self.assertEqual(fib(1), 1)
+        self.assertEqual(fib(2), 1)
+
+    def test_mid_range_numbers(self):
+        """ Testing for mid range numbers
+        """
+
+        self.assertEqual(fib(5), 5)
+        self.assertEqual(fib(6), 8)
+        self.assertEqual(fib(13), 233)
+
+    def test_big_numbers(self):
+        """ Testing for big numbers
+        """
+
+        self.assertEqual(fib(35), 9227465)  # ok
+        # self.assertEqual(fib(53), 53316291173) # don't test, runtime too long in recursion
+        # self.assertEqual(fib(100), 354224848179261915075) # don't test
 
 
 if __name__ == "__main__":
