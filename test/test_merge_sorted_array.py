0753de91947dd9784e09ae7db83da10ad76f0094
podmar <marta.podziewska@gmail.com>
2022-11-15 13:27:17 +0100

    first happy path test

--- test/test_merge_sorted_array.py
+++ test/test_merge_sorted_array.py
@@ -14,14 +14,13 @@ class TestMergeSortedArray(unittest.TestCase):
         """Tests is the output is None
         """
 
-        self.assertIsNone(merge(self.nums1,
-                                self.m,
-                                self.nums2,
-                                self.n))
+        self.assertIsNone(merge(self.nums1, self.m, self.nums2, self.n))
 
     def test_(self):
-        """ 
+        """ Testing first happy path case
         """
+        merge(self.nums1, self.m, self.nums2, self.n)
+        self.assertEquals(self.nums1, [1, 2, 2, 3, 5, 6])
 
 
 if __name__ == "__main__":
