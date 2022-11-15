13739ae74f01fe2a877b301379f29e2eff421a38
podmar <marta.podziewska@gmail.com>
2022-11-15 18:37:09 +0100

    passing the currently written tests, not a final solution

--- challenges/merge_sorted_array.py
+++ challenges/merge_sorted_array.py
@@ -49,13 +49,20 @@ def merge(nums1: list[int], m: int, nums2: list[int], n: int):
                 nums1[m + n - 1] = nums1[m-1]
                 m -= 1
 
+        while n > 0:
+            nums1[m] = nums2[n-1]
+            n -= 1
+
+        print(f"m={m}, n={n}")
+
         return
 
 
 if __name__ == "__main__":
 
-    nums1 = [1, 2, 3, 0, 0, 0]
-    m = 3
-    nums2 = [2, 5, 6]
-    n = 3
+    nums1 = [2, 0]
+    m = 1
+    nums2 = [1]
+    n = 1
     merge(nums1, m, nums2, n)
+    # print()
