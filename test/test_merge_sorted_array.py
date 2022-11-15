ada4ae5bce416ca30f8743301be6e353a2f71c4f
podmar <marta.podziewska@gmail.com>
2022-11-15 13:20:06 +0100

    set up method added

--- test/test_merge_sorted_array.py
+++ test/test_merge_sorted_array.py
@@ -4,11 +4,20 @@ from challenges.merge_sorted_array import merge
 
 
 class TestMergeSortedArray(unittest.TestCase):
+    def setUp(self):
+        self.nums1 = [1, 2, 3, 0, 0, 0]
+        self.m = 3
+        self.nums2 = [2, 5, 6]
+        self.n = 3
+
     def test_output_is_none(self):
         """Tests is the output is None
         """
 
-        self.assertIsNone(merge())
+        self.assertIsNone(merge(self.nums1,
+                                self.m,
+                                self.nums2,
+                                self.n))
 
     def test_(self):
         """ 
