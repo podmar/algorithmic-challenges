d76837609b8e357485cc8b60924c110b74ab66b4
podmar <marta.podziewska@gmail.com>
2022-11-15 18:21:18 +0100

    added corner cases

--- test/test_merge_sorted_array.py
+++ test/test_merge_sorted_array.py
@@ -51,5 +51,47 @@ class TestMergeNum1Len0(unittest.TestCase):
         self.assertEqual(self.nums1, [1])
 
 
+class TestMergeNum1Longer(unittest.TestCase):
+    def setUp(self):
+        self.nums1 = [0, 3, 6, 8, 8, 9, 0, 0]
+        self.m = 6
+        self.nums2 = [1, 3]
+        self.n = 2
+
+    def test_len_num1_longer(self):
+        """ Testing with m > n
+        """
+        merge(self.nums1, self.m, self.nums2, self.n)
+        self.assertEqual(self.nums1, [0, 1, 3, 3, 6, 8, 8, 9])
+
+
+class TestMergeNum2Longer(unittest.TestCase):
+    def setUp(self):
+        self.nums1 = [1, 3, 0, 0, 0, 0, 0]
+        self.m = 2
+        self.nums2 = [0, 6, 6, 8, 8]
+        self.n = 5
+
+    def test_len_num2_longer(self):
+        """ Testing n > m
+        """
+        merge(self.nums1, self.m, self.nums2, self.n)
+        self.assertEqual(self.nums1, [0, 1, 3, 6, 6, 8, 8])
+
+
+class TestMergeBothArraysLen1(unittest.TestCase):
+    def setUp(self):
+        self.nums1 = [2, 0]
+        self.m = 1
+        self.nums2 = [1]
+        self.n = 1
+
+    def test_both_arrays_len_1(self):
+        """ Testing n = m = 1
+        """
+        merge(self.nums1, self.m, self.nums2, self.n)
+        self.assertEqual(self.nums1, [1, 2])
+
+
 if __name__ == "__main__":
     unittest.main()
