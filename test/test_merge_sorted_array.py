464899b019ace9099a761a849714b01dde325ea7
podmar <marta.podziewska@gmail.com>
2022-11-15 17:30:29 +0100

    2 additional corner cases with array lenghts = 0

--- test/test_merge_sorted_array.py
+++ test/test_merge_sorted_array.py
@@ -3,7 +3,7 @@ import unittest
 from challenges.merge_sorted_array import merge
 
 
-class TestMergeSortedArray(unittest.TestCase):
+class TestMergeBasicCase(unittest.TestCase):
     def setUp(self):
         self.nums1 = [1, 2, 3, 0, 0, 0]
         self.m = 3
@@ -16,11 +16,39 @@ class TestMergeSortedArray(unittest.TestCase):
 
         self.assertIsNone(merge(self.nums1, self.m, self.nums2, self.n))
 
-    def test_(self):
+    def test_happy_path(self):
         """ Testing first happy path case
         """
         merge(self.nums1, self.m, self.nums2, self.n)
         self.assertEquals(self.nums1, [1, 2, 2, 3, 5, 6])
+
+
+class TestMergeNum2Len0(unittest.TestCase):
+    def setUp(self):
+        self.nums1 = [1]
+        self.m = 1
+        self.nums2 = []
+        self.n = 0
+
+    def test_len_num2_0(self):
+        """ Testing second array len = 0
+        """
+        merge(self.nums1, self.m, self.nums2, self.n)
+        self.assertEquals(self.nums1, [1])
+
+
+class TestMergeNum1Len0(unittest.TestCase):
+    def setUp(self):
+        self.nums1 = [0]
+        self.m = 0
+        self.nums2 = [1]
+        self.n = 1
+
+    def test_len_num1_0(self):
+        """ Testing first array len = 0
+        """
+        merge(self.nums1, self.m, self.nums2, self.n)
+        self.assertEquals(self.nums1, [1])
 
 
 if __name__ == "__main__":
