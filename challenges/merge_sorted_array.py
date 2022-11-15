36654cb8b8e53d20958ce4e51b4058ab330e5d01
podmar <marta.podziewska@gmail.com>
2022-11-15 17:32:46 +0100

    additional case for m = 0

--- challenges/merge_sorted_array.py
+++ challenges/merge_sorted_array.py
@@ -38,15 +38,18 @@
 
 def merge(nums1: list[int], m: int, nums2: list[int], n: int):
 
-    while m > 0 and n > 0:
-        if nums1[m-1] < nums2[n-1]:
-            nums1[m + n - 1] = nums2[n-1]
-            n -= 1
-        else:
-            nums1[m + n - 1] = nums1[m-1]
-            m -= 1
+    if m == 0:
+        nums1 = nums2
+    else:
+        while m > 0 and n > 0:
+            if nums1[m-1] < nums2[n-1]:
+                nums1[m + n - 1] = nums2[n-1]
+                n -= 1
+            else:
+                nums1[m + n - 1] = nums1[m-1]
+                m -= 1
 
-    return
+        return
 
 
 if __name__ == "__main__":
