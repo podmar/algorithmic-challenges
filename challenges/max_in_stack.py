8c917a10249d3c75c4aeb2768a5fbb3d0a6b0bc5
podmar <marta.podziewska@gmail.com>
2022-11-16 18:34:33 +0100

    new problem for max item in a stack in o(1)

---
+++ challenges/max_in_stack.py
@@ -0,0 +1,9 @@
+# design an algorithm that can return the maximum item of a stack in O(1) running time complexity. We can use O(N) extra memory!
+# Hint: we can use another stack to track the max item!
+
+def find_max_item(stack: list[int]) -> int:
+    """the function finds the max element in a stack in o(1) running time.
+    """
+    max_item = int
+
+    return max_item
