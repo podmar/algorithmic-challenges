38e1cce31267549b7713163bfd95ed4fb2c8f7a2
podmar <marta.podziewska@gmail.com>
2022-11-17 18:32:04 +0100

    dequeue, check lenght methods

--- challenges/queue_implementation.py
+++ challenges/queue_implementation.py
@@ -16,11 +16,22 @@ class TwoStackQueue:
         if len(self.dequeue_stack) != 0:
             while len(self.dequeue_stack) > 0:
                 item = self.dequeue_stack.pop()
-                self.equeue_stack.append(item)
-        self.equeue_stack.append(new_item)
+                self.enqueue_stack.append(item)
+        self.enqueue_stack.append(new_item)
 
     def dequeue(self):
-        return
+        if self.is_empty():
+            raise Exception("The queue is empty, cannot dequeue.")
+        elif len(self.enqueue_stack) != 0:
+            while len(self.enqueue_stack) > 0:
+                item = self.enqueue_stack.pop()
+                self.dequeue_stack.append(item)
+        dequeued_item = self.dequeue_stack.pop()
+        return dequeued_item
+
+    def get_length(self):
+        queue_length = max(self.enqueue_stack, self.dequeue_stack)
+        return queue_length
 
 
 class RecursiveQueue:
