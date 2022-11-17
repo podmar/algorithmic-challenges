44ee476f09539656f82c21188b2cee753a28cfe8
podmar <marta.podziewska@gmail.com>
2022-11-17 17:11:29 +0100

    removed redundant function declaration

--- challenges/max_in_stack.py
+++ challenges/max_in_stack.py
@@ -1,14 +1,6 @@
 # design an algorithm that can return the maximum item of a stack in O(1) running time complexity. We can use O(N) extra memory!
 # Hint: we can use another stack to track the max item!
 
-def find_max_item(stack: list[int]) -> int:
-    """the function finds the max element in a stack in o(1) running time.
-    """
-    max_items = int
-
-    return max_items
-
-
 class StackWithMaxValue:
     def __init__(self):
         self.stack = []
