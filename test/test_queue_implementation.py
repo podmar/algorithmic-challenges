99aecb64f31a3386934f08aa66b9a100372a69aa
podmar <marta.podziewska@gmail.com>
2022-11-17 19:20:17 +0100

    enqueue / dequeue operations ok, tests passing

--- test/test_queue_implementation.py
+++ test/test_queue_implementation.py
@@ -7,7 +7,6 @@ class TestTwoStackQueue(unittest.TestCase):
     def setUp(self):
         self.queue = TwoStackQueue()
         self.queue.enqueue(19)
-        self.queue.dequeue()
         self.queue.enqueue(1)
         self.queue.enqueue(14)
 
@@ -15,12 +14,23 @@ class TestTwoStackQueue(unittest.TestCase):
         """Tests is output of enqueue/ dequeue, is_empty, get_genth is correct
         """
 
-        # self.assertEqual(self.queue.dequeue(), 14)
-        # self.assertEqual(self.queue.get_length, 1)
+        self.assertEqual(self.queue.dequeue(), 19)
         self.assertEqual(self.queue.dequeue(), 1)
-        self.assertTrue(self.queue.is_empty())
-        self.assertRaises(Exception, self.queue.dequeue())
+        self.assertEqual(self.queue.dequeue(), 14)
 
+        self.queue.enqueue(32)
+        self.assertEqual(self.queue.dequeue(), 32)
 
+        self.queue.enqueue(0)
+        self.queue.enqueue(71)
+        self.queue.enqueue(3)
+        self.assertEqual(self.queue.dequeue(), 0)
+
+        # self.assertRaises(
+        #     Exception("The queue is empty, cannot dequeue."), self.queue.dequeue())
+
+        # self.assertFalse(self.queue.is_empty())
+        # self.assertEqual(self.queue.dequeue(), 19)
+        # self.assertEqual(self.queue.get_length, 2)
 if __name__ == "__main__":
     unittest.main()
