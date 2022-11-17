2e1045c4f4418258cd3677352323519daf856fd7
podmar <marta.podziewska@gmail.com>
2022-11-17 19:03:05 +0100

    bugfixing, print statements

--- challenges/queue_implementation.py
+++ challenges/queue_implementation.py
@@ -16,17 +16,28 @@ class TwoStackQueue:
         if len(self.dequeue_stack) != 0:
             while len(self.dequeue_stack) > 0:
                 item = self.dequeue_stack.pop()
+                print(f"moving {item} to the enqueue stack")
                 self.enqueue_stack.append(item)
+
+        print(f"Current enqueue stack: {self.enqueue_stack}")
         self.enqueue_stack.append(new_item)
+        print(
+            f"Enque stack after appending {new_item} is: {self.enqueue_stack}")
 
     def dequeue(self):
         if self.is_empty():
             raise Exception("The queue is empty, cannot dequeue.")
         elif len(self.enqueue_stack) != 0:
+            print("Dequeuing. items in enqueing stack, moving.")
             while len(self.enqueue_stack) > 0:
                 item = self.enqueue_stack.pop()
+                print(f"moving {item} to the dequeue stack")
                 self.dequeue_stack.append(item)
+
+        print(f"Current dequeue stack: {self.dequeue_stack}")
         dequeued_item = self.dequeue_stack.pop()
+        print(
+            f"Dequeue stack after dequeueing {dequeued_item} is: {self.dequeue_stack}")
         return dequeued_item
 
     def get_length(self):
--- test/test_queue_implementation.py
+++ test/test_queue_implementation.py
@@ -6,13 +6,20 @@ from challenges.queue_implementation import TwoStackQueue, RecursiveQueue
 class TestTwoStackQueue(unittest.TestCase):
     def setUp(self):
         self.queue = TwoStackQueue()
+        self.queue.enqueue(19)
+        self.queue.dequeue()
+        self.queue.enqueue(1)
         self.queue.enqueue(14)
 
-    def test_dequeue_output(self):
-        """Tests is the output of dequeue function is correct
+    def test_all_methods(self):
+        """Tests is output of enqueue/ dequeue, is_empty, get_genth is correct
         """
 
-        self.assertEqual(self.queue.dequeue(), 14)
+        # self.assertEqual(self.queue.dequeue(), 14)
+        # self.assertEqual(self.queue.get_length, 1)
+        self.assertEqual(self.queue.dequeue(), 1)
+        self.assertTrue(self.queue.is_empty())
+        self.assertRaises(Exception, self.queue.dequeue())
 
 
 if __name__ == "__main__":
