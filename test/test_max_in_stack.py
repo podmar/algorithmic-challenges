bde569ed07defc9138c9f72e5288323f5d61c125
podmar <marta.podziewska@gmail.com>
2022-11-17 16:43:30 +0100

    wrote setUp, first test checking output type changed to take a class

--- test/test_max_in_stack.py
+++ test/test_max_in_stack.py
@@ -1,17 +1,18 @@
 import unittest
 
-from challenges.max_in_stack import max_in_stack
+from challenges.max_in_stack import StackWithMaxValue
 
 
 class TestMaxInStackOutputType(unittest.TestCase):
     def setUp(self):
-        self.stack
+        self.stack = StackWithMaxValue()
+        self.stack.push(14)
 
     def test_output_is_int(self):
         """Tests is the output is int
         """
 
-        self.assertIsInstance(max_in_stack(self.stack), int)
+        self.assertIsInstance(self.stack.peek_max_value(), int)
 
     def test_happy_path(self):
         """ Testing first happy path case
