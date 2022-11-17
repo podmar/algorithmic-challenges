69fab097f137ea420e9b0963b7e0be63ac763e3f
podmar <marta.podziewska@gmail.com>
2022-11-17 16:39:16 +0100

    created the stack class

--- challenges/max_in_stack.py
+++ challenges/max_in_stack.py
@@ -4,6 +4,42 @@
 def find_max_item(stack: list[int]) -> int:
     """the function finds the max element in a stack in o(1) running time.
     """
-    max_item = int
+    max_items = int
 
-    return max_item
+    return max_items
+
+
+class StackWithMaxValue:
+    def __init__(self):
+        self.stack = []
+        self.max_values = []
+
+    def is_empty(self):
+        if len(self.stack) == 0:
+            return True
+
+    def peek_max_value(self):
+        if not self.is_empty():
+            return self.max_values[- 1]
+        else:
+            return "Stack is empty"
+
+    def push(self, value):
+        if self.is_empty():
+            self.max_values.append(value)
+        else:
+            self.max_values.append(max(value, self.max_values[- 1]))
+
+        self.stack.append(value)
+
+    def pop(self):
+        if not self.is_empty():
+            popped_value = self.stack[-1]
+            self.stack = self.stack[:-1]
+            self.max_values = self.max_values[:-1]
+        else:
+            popped_value = None
+        return popped_value
+
+    def get_length(self):
+        return len(self.stack)
