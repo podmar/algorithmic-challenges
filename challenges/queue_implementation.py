c0049f5954d181d5033cd8dedbc4b48a1e07a4b5
podmar <marta.podziewska@gmail.com>
2022-11-18 12:16:42 +0100

    modified get_lenght

--- challenges/queue_implementation.py
+++ challenges/queue_implementation.py
@@ -16,32 +16,32 @@ class TwoStackQueue:
         if len(self.dequeue_stack) != 0:
             while len(self.dequeue_stack) > 0:
                 item = self.dequeue_stack.pop()
-                print(f"moving {item} to the enqueue stack")
+                # print(f"moving {item} to the enqueue stack")
                 self.enqueue_stack.append(item)
 
-        print(f"Current enqueue stack: {self.enqueue_stack}")
+        # print(f"Current enqueue stack: {self.enqueue_stack}")
         self.enqueue_stack.append(new_item)
-        print(
-            f"Enque stack after appending {new_item} is: {self.enqueue_stack}")
+        # print(
+        #     f"Enque stack after appending {new_item} is: {self.enqueue_stack}")
 
     def dequeue(self):
         if self.is_empty():
             raise Exception("The queue is empty, cannot dequeue.")
         elif len(self.enqueue_stack) != 0:
-            print("Dequeuing. items in enqueing stack, moving.")
+            # print("Dequeuing. items in enqueing stack, moving.")
             while len(self.enqueue_stack) > 0:
                 item = self.enqueue_stack.pop()
-                print(f"moving {item} to the dequeue stack")
+                # print(f"moving {item} to the dequeue stack")
                 self.dequeue_stack.append(item)
 
-        print(f"Current dequeue stack: {self.dequeue_stack}")
+        # print(f"Current dequeue stack: {self.dequeue_stack}")
         dequeued_item = self.dequeue_stack.pop()
-        print(
-            f"Dequeue stack after dequeueing {dequeued_item} is: {self.dequeue_stack}")
+        # print(
+        # f"Dequeue stack after dequeueing {dequeued_item} is: {self.dequeue_stack}")
         return dequeued_item
 
     def get_length(self):
-        queue_length = max(self.enqueue_stack, self.dequeue_stack)
+        queue_length = len(self.dequeue_stack) + len(self.enqueue_stack)
         return queue_length
 
 
