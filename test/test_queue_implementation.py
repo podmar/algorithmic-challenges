e78eaae2fb3f22927476d2e5d07b9c2348deb2b0
podmar <marta.podziewska@gmail.com>
2022-11-18 12:52:45 +0100

    tests for recursive queue

--- test/test_queue_implementation.py
+++ test/test_queue_implementation.py
@@ -38,5 +38,40 @@ class TestTwoStackQueue(unittest.TestCase):
             assert_error.exception.args[0], "The queue is empty, cannot dequeue.")
 
 
+class TestRecursiveQueue(unittest.TestCase):
+    def setUp(self):
+        self.recursive_queue = RecursiveQueue()
+        self.recursive_queue.enqueue(19)
+        self.recursive_queue.enqueue(1)
+        self.recursive_queue.enqueue(14)
+
+    def test_all_methods(self):
+        """Tests is output of enqueue/ dequeue, is_empty, get_genth is correct for the 2 stack queue
+        """
+
+        self.assertEqual(self.recursive_queue.get_length(), 3)
+
+        self.assertEqual(self.recursive_queue.dequeue(), 19)
+        self.assertEqual(self.recursive_queue.dequeue(), 1)
+        self.assertEqual(self.recursive_queue.dequeue(), 14)
+
+        self.assertEqual(self.recursive_queue.get_length(), 0)
+
+        self.recursive_queue.enqueue(32)
+        self.assertEqual(self.recursive_queue.dequeue(), 32)
+        self.assertTrue(self.recursive_queue.is_empty())
+
+        self.recursive_queue.enqueue(0)
+        self.recursive_queue.enqueue(3)
+        self.assertFalse(self.recursive_queue.is_empty())
+        self.assertEqual(self.recursive_queue.dequeue(), 0)
+        self.assertEqual(self.recursive_queue.dequeue(), 3)
+
+        with self.assertRaises(Exception) as assert_error:
+            self.recursive_queue.dequeue()
+        self.assertEqual(
+            assert_error.exception.args[0], "The queue is empty, cannot dequeue.")
+
+
 if __name__ == "__main__":
     unittest.main()
