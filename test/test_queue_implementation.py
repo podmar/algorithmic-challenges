709fcea8c2153f7865da89905544ec953328944e
podmar <marta.podziewska@gmail.com>
2022-11-18 12:20:57 +0100

    testing lenght (missing bracket fixed), exception when dequeuing an empty queue, modified docstring

--- test/test_queue_implementation.py
+++ test/test_queue_implementation.py
@@ -11,26 +11,32 @@ class TestTwoStackQueue(unittest.TestCase):
         self.queue.enqueue(14)
 
     def test_all_methods(self):
-        """Tests is output of enqueue/ dequeue, is_empty, get_genth is correct
+        """Tests is output of enqueue/ dequeue, is_empty, get_genth is correct for the 2 stack queue
         """
+
+        self.assertEqual(self.queue.get_length(), 3)
 
         self.assertEqual(self.queue.dequeue(), 19)
         self.assertEqual(self.queue.dequeue(), 1)
         self.assertEqual(self.queue.dequeue(), 14)
 
+        self.assertEqual(self.queue.get_length(), 0)
+
         self.queue.enqueue(32)
         self.assertEqual(self.queue.dequeue(), 32)
+        self.assertTrue(self.queue.is_empty())
 
         self.queue.enqueue(0)
-        self.queue.enqueue(71)
         self.queue.enqueue(3)
+        self.assertFalse(self.queue.is_empty())
         self.assertEqual(self.queue.dequeue(), 0)
+        self.assertEqual(self.queue.dequeue(), 3)
 
-        # self.assertRaises(
-        #     Exception("The queue is empty, cannot dequeue."), self.queue.dequeue())
+        with self.assertRaises(Exception) as assert_error:
+            self.queue.dequeue()
+        self.assertEqual(
+            assert_error.exception.args[0], "The queue is empty, cannot dequeue.")
 
-        # self.assertFalse(self.queue.is_empty())
-        # self.assertEqual(self.queue.dequeue(), 19)
-        # self.assertEqual(self.queue.get_length, 2)
+
 if __name__ == "__main__":
     unittest.main()
