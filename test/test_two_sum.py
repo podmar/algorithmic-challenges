9f953bed849b490be5a27fad8b0803aec2f215bd
podmar <marta.podziewska@gmail.com>
2022-11-21 14:55:11 +0100

    new task

---
+++ test/test_two_sum.py
@@ -0,0 +1,14 @@
+import unittest
+
+from challenges.two_sum import solution
+
+
+class TestTwoSum(unittest.TestCase):
+
+    def test_output(self):
+        """
+        """
+
+
+if __name__ == "__main__":
+    unittest.main()
