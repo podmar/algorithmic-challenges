6e40f1b17170fe51a5f8451448f413b46efbd12f
podmar <marta.podziewska@gmail.com>
2022-11-22 19:38:17 +0100

    tests for the basic cases, passing

--- test/test_two_sum.py
+++ test/test_two_sum.py
@@ -6,8 +6,11 @@ from challenges.two_sum import solution
 class TestTwoSum(unittest.TestCase):
 
     def test_output(self):
+        """Testing the base cases from the example
         """
-        """
+        self.assertEqual(solution([2, 7, 11, 15], 9), [0, 1])
+        self.assertEqual(solution([3, 2, 4], 6), [1, 2])
+        self.assertEqual(solution([3, 3], 6), [0, 1])
 
 
 if __name__ == "__main__":
