db46178858369978eac8db12b902af6a3e42a461
podmar <marta.podziewska@gmail.com>
2022-11-22 19:54:44 +0100

    o(n2) solution

--- challenges/two_sum.py
+++ challenges/two_sum.py
@@ -31,6 +31,12 @@ def solution(nums: list[int], target: int) -> list[int]:
     """
     """
 # counting a sum of two consecutive elements
-    for i in range(len(nums)-1):
-        if nums[i] + nums[i + 1] == target:
-            return [i, i+1]
+    # for i in range(len(nums)-1):
+    #     if nums[i] + nums[i + 1] == target:
+    #         return [i, i+1]
+
+# solution with 2 loops - o(n2)
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
