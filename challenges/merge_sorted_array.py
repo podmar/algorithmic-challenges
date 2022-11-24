35d0d16113308f635f5aa67fba4261567742d8b1
podmar <marta.podziewska@gmail.com>
2022-11-24 21:18:03 +0100

    removed redundant code

--- challenges/merge_sorted_array.py
+++ challenges/merge_sorted_array.py
@@ -38,36 +38,21 @@
 
 def merge(nums1: list[int], m: int, nums2: list[int], n: int):
 
-    if m == 0:
-        nums1[:] = nums2[:n]
-    else:
-        while m > 0 and n > 0:
-            if nums1[m-1] < nums2[n-1]:
-                nums1[m + n - 1] = nums2[n-1]
-                n -= 1
-            elif nums1[m-1] > nums2[n-1]:
-                nums1[m + n - 1] = nums1[m-1]
-                m -= 1
-            else:
-                nums1[m + n - 1] = nums1[m-1]
-                m -= 1
-                nums1[m + n - 1] = nums2[n-1]
-                n -= 1
-
-        while n > 0:
+    while m > 0 and n > 0:
+        if nums1[m-1] < nums2[n-1]:
+            nums1[m + n - 1] = nums2[n-1]
+            n -= 1
+        elif nums1[m-1] > nums2[n-1]:
+            nums1[m + n - 1] = nums1[m-1]
+            m -= 1
+        else:
+            nums1[m + n - 1] = nums1[m-1]
+            m -= 1
             nums1[m + n - 1] = nums2[n-1]
             n -= 1
 
-        print(f"m={m}, n={n}")
+    while n > 0:
+        nums1[m + n - 1] = nums2[n-1]
+        n -= 1
 
-        return
-
-
-if __name__ == "__main__":
-
-    nums1 = [2, 0]
-    m = 1
-    nums2 = [1]
-    n = 1
-    merge(nums1, m, nums2, n)
-    # print()
+    return
