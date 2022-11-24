5f7d380cfc7495e83bf1118de82d3a8692a5c5da
podmar <marta.podziewska@gmail.com>
2022-11-24 21:16:51 +0100

    added the case when elements are equal plus fixed the index if n bigger than m

--- challenges/merge_sorted_array.py
+++ challenges/merge_sorted_array.py
@@ -45,12 +45,17 @@ def merge(nums1: list[int], m: int, nums2: list[int], n: int):
             if nums1[m-1] < nums2[n-1]:
                 nums1[m + n - 1] = nums2[n-1]
                 n -= 1
+            elif nums1[m-1] > nums2[n-1]:
+                nums1[m + n - 1] = nums1[m-1]
+                m -= 1
             else:
                 nums1[m + n - 1] = nums1[m-1]
                 m -= 1
+                nums1[m + n - 1] = nums2[n-1]
+                n -= 1
 
         while n > 0:
-            nums1[m] = nums2[n-1]
+            nums1[m + n - 1] = nums2[n-1]
             n -= 1
 
         print(f"m={m}, n={n}")
