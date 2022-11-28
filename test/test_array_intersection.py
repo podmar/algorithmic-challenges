d7932e8b2c01addc64210046137674c09e8bf02f
podmar <marta.podziewska@gmail.com>
2022-11-28 19:09:49 +0100

    tests for output type and the happy path

--- test/test_array_intersection.py
+++ test/test_array_intersection.py
@@ -5,10 +5,16 @@ from challenges.array_intersection import solution
 
 class TestArrayIntersection(unittest.TestCase):
 
+    def test_result_type(self):
+        """Testing the return type is an array
+        """
+        self.assertIsInstance(solution([], []), list)
+
     def test_happy_path(self):
         """Testing the base cases from the example
         """
-        self.assertEqual(solution([], []), [])
+        self.assertEqual(solution[1, 2, 2, 1], [2, 2], [2, 2])
+        self.assertEqual(solution[4, 9, 5], [9, 4, 9, 8, 4], [4, 9])
 
 
 if __name__ == "__main__":
