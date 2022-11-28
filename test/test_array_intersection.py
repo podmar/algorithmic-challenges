0fb9c1b18a73ff02033f35c191f3a253c3e87ce0
podmar <marta.podziewska@gmail.com>
2022-11-28 19:37:25 +0100

    fixing result in an assertion

--- test/test_array_intersection.py
+++ test/test_array_intersection.py
@@ -13,8 +13,8 @@ class TestArrayIntersection(unittest.TestCase):
     def test_happy_path(self):
         """Testing the base cases from the example
         """
-        self.assertEqual(solution[1, 2, 2, 1], [2, 2], [2, 2])
-        self.assertEqual(solution[4, 9, 5], [9, 4, 9, 8, 4], [4, 9])
+        self.assertEqual(solution([1, 2, 2, 1], [2, 2]), [2, 2])
+        self.assertEqual(solution([4, 9, 5], [9, 4, 9, 8, 4]), [9, 4])
 
 
 if __name__ == "__main__":
