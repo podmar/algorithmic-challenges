57b6c2c86f62170775d3077a3c7040b46fd3aa80
podmar <marta.podziewska@gmail.com>
2022-12-15 13:23:38 +0100

    timer decorator in nanoseconds

--- utilities/timer.py
+++ utilities/timer.py
@@ -1,13 +1,13 @@
-from time import perf_counter
+from time import perf_counter_ns
 
 def timer(my_func):
 
     def wrapper(*args, **kwargs):
-        start = perf_counter()
+        start = perf_counter_ns()
         result = my_func(*args, **kwargs)
-        finish = perf_counter()
+        finish = perf_counter_ns()
         execution_time = finish - start
-        print(f'Function {my_func.__name__} ran in {execution_time} seconds.')
+        print(f'Function {my_func.__name__} ran in {execution_time} nanoseconds:\nargs: {args}\nkwargs: {kwargs}')
         return result
 
     return wrapper
