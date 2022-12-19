99bdde19c99fed3bbcdbe5bcc7787ce84e994dd2
podmar <marta.podziewska@gmail.com>
2022-12-19 19:42:37 +0100

    applied wraps decorator and added testing code

--- utilities/logger.py
+++ utilities/logger.py
@@ -1,10 +1,26 @@
+from functools import wraps
 import logging
+
 
 def logger(my_func):
     logging.basicConfig(level=logging.INFO)
 
+    @wraps(my_func)
     def wrapper(*args, **kwargs):
-        logging.info(f'Function {my_func.__name__} ran with:\nargs: {args}\nkwargs: {kwargs}')
+        logging.info(
+            f'Function {my_func.__name__} ran with:\nargs: {args}\nkwargs: {kwargs}')
         return my_func(*args, **kwargs)
 
     return wrapper
+
+
+@logger
+def test(n):
+    sum = 0
+    for i in range(n):
+        sum += i
+    return sum
+
+
+if __name__ == '__main__':
+    test(6)
--- utilities/timer.py
+++ utilities/timer.py
@@ -1,13 +1,32 @@
 from time import perf_counter_ns
+from functools import wraps
+
+from logger import logger
+
 
 def timer(my_func):
 
+    @wraps(my_func)
     def wrapper(*args, **kwargs):
         start = perf_counter_ns()
         result = my_func(*args, **kwargs)
         finish = perf_counter_ns()
         execution_time = finish - start
-        print(f'Function {my_func.__name__} ran in {execution_time} nanoseconds:\nargs: {args}\nkwargs: {kwargs}')
+        print(
+            f'Function {my_func.__name__} ran in {execution_time} nanoseconds:\nargs: {args}\nkwargs: {kwargs}')
         return result
 
     return wrapper
+
+
+@logger
+@timer
+def test(n):
+    sum = 0
+    for i in range(n):
+        sum += i
+    return sum
+
+
+if __name__ == '__main__':
+    test(6)
